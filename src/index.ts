eebc0a154ea6f450754b3724a2c077360a6bfe3c
Alex Cabrera <uy.acabrera@gmail.com>
2018-09-17 19:38:22 -0300

    .env file added

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { SocketEventsService } from './socket/socket-event.service';
 
 export class Main {
     constructor() {
-        let socketService = new SocketService('http://localhost:3001');
+        let url = process.env.GAME_ENGINE_URL || 'http://localhost:3001';
+        let socketService = new SocketService(url);
         socketService.createSocket();
 
         let socketEventsService = new SocketEventsService(socketService.getSocket());
